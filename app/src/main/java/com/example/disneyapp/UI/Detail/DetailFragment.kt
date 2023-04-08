948a1ebaa8a87c48ba7af3e5d4cbc1c775d644d6
onur mert boybek <onurboybek@gmail.com>
2023-04-08 17:06:10 +0300

    detail page is ready 2

--- app/src/main/java/com/example/disneyapp/UI/Detail/DetailFragment.kt
+++ app/src/main/java/com/example/disneyapp/UI/Detail/DetailFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.disneyapp.Model.DisneyData
+import com.example.disneyapp.R
 import com.example.disneyapp.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +17,9 @@ class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
 
+    private val viewModel by lazy {
+        ViewModelProvider(this, defaultViewModelProviderFactory).get(DetailViewModel::class.java)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,15 +28,38 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getDisneyData(getID())
+        getDisneyData()
+    }
 
-        println("id: " + getID())
+    private fun getDisneyData(){
+        viewModel.disneyData.observe(viewLifecycleOwner, {
+            item->
+            if (item != null){
+                binding.progressBar.visibility = View.GONE
+                binding.detailFragment.visibility = View.VISIBLE
+                createDisneyCharacter(item)
+            }else{
+                binding.progressBar.visibility = View.VISIBLE
+                binding.detailFragment.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun createDisneyCharacter(disneyData: DisneyData) {
+
+        Glide.with(requireContext())
+            .load(disneyData.imageUrl)
+            .error(R.drawable.disney_img)
+            .into(binding.imageView2)
+
+        binding.textView2.setText(disneyData.name)
     }
 
     private fun getID(): Int{
